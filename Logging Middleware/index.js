63cb08175a65ba7497ebd5488f2994ca4d9eb2ae
Hemanth Reddy Komma <143542784+hemanthreddy-komma@users.noreply.github.com>
2025-09-04 11:14:56 +0530

    Update index.js

---
+++ Logging Middleware/index.js
@@ -0,0 +1,7 @@
+/**
+ * Logging Middleware Entry Point
+ * Exports all logging utilities for easy import
+ */
+
+export { default as logger, Logger } from './logger.js';
+export { default as useLogger } from './useLogger.js';
